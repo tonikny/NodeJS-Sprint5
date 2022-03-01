c62a64c0c8a77a1724cb12d6d415f51933f77f69
tonikny <dalecanya@gmail.com>
2022-03-01 21:59:56 +0100

    client: component sala amb prova socket.io

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SalaComponent } from './sala/sala.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sala', component: SalaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/sala/sala.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { XatService } from './xat.service';
+
+@Component({
+  selector: 'app-sala',
+  templateUrl: './sala.component.html',
+  styleUrls: ['./sala.component.sass']
+})
+export class SalaComponent implements OnInit {
+  newMessage: string = '';
+  messageList: string[] = [];
+
+  constructor(private xatService: XatService){
+
+  }
+
+  ngOnInit(){
+    this.xatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+    this.xatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
+}
